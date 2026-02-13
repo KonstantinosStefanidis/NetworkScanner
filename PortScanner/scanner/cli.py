f6877115ca4571f5d88d4a35076cd1c7c7d7e3df
KonstantinosStefanidis <viviotic01@gmail.com>
2026-02-13 23:09:10 +0200

    Reformated the displaying of ports. Scanner will now only list open and filtered ports.

--- PortScanner/scanner/cli.py
+++ PortScanner/scanner/cli.py
@@ -5,22 +5,44 @@ from scanner.syn_scan import syn_scan
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", help="Target IP address to scan", required=True)
-    parser.add_argument("-p", "--ports", help="Comma-separated list of ports to scan")
-    parser.add_argument("-s", "--start", help="Start of port range to scan" ,type=int, default=1)
-    parser.add_argument("-e", "--end", help="End of port range to scan", type=int, default=1024)
+    parser.add_argument("-p", "--ports", help="Comma-separated list of ports to scan", type=int)
+    parser.add_argument("-s", "--start", help="Start of port range to scan" ,type=int)
+    parser.add_argument("-e", "--end", help="End of port range to scan", type=int)
     args = parser.parse_args()
 
     target = args.target
     targeted_port = args.ports
     start_port = args.start
     end_port = args.end
+    open_ports = []
+    filtered_ports = []
 
     if targeted_port:
+        #BUG: This doesnt work as intented, it only takes the first port and ignores the rest. 
+        #Need to split the string by comma and convert each to int.
         ports = [int(p.strip()) for p in targeted_port.split(",")]
         for port in ports:
             result = syn_scan(target, port)
-            print(f"Port {port}: {result}")
+            if result == "OPEN":
+                open_ports.append(port)
+            if result == "FILTERED":
+                filtered_ports.append(port)
 
-    for port in range(start_port, end_port + 1):
-        result = syn_scan(target, port)
-        print(f"Port {port}: {result}")
+        for port in range(start_port, end_port + 1):
+            result = syn_scan(target, port)
+            if result == "OPEN":
+                open_ports.append(port)
+            if result == "FILTERED":
+                filtered_ports.append(port)
+
+    print("Scan completed.")
+
+    print(f"Open ports:{len(open_ports)}")
+    for port in open_ports:
+        print(f"Port {port} is OPEN")
+
+    print(f"Filtered ports:{len(filtered_ports)}")
+    for port in filtered_ports:
+        print(f"Port {port} is FILTERED")
+    
+
